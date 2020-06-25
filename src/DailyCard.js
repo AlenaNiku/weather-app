2dc7cc2d9bd143a2ef0ebe0a41185e1f0cd16772
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-24 22:26:28 -0400

    mapping through the data and creating a daily card componnet for each object in the array

---
+++ src/DailyCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DailyCard = () => {
+    return (
+        <div>
+            <h1>Hi! I'm daily Card!</h1>
+        </div>
+    )
+}
+
+export default DailyCard
--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react'
+import DailyCard from './DailyCard';
 
 class WeekContainer extends Component {
-
-    state = {
-        fullData: [],
-        dailyData: []
-    }
+  state = {
+    fullData: [],
+    dailyData: [],
+  };
 
   componentDidMount = () => {
-
     const apiKey = "bb92bc497a64f5f4cdcd15d73f3e55ee";
 
     const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=10022&units=imperial&APPID=${apiKey}`;
 
     fetch(weatherURL)
       .then((res) => res.json())
-      .then(data => {
-          const dailyData = data.list.filter((reading) =>
-            reading.dt_txt.includes("18:00:00")
-          )
-          this.setState({
-              fullData: data.list,
-              dailyData: dailyData
-          }, () => console.log(this.state))
+      .then((data) => {
+        const dailyData = data.list.filter((reading) =>
+          reading.dt_txt.includes("18:00:00")
+        );
+        this.setState(
+          {
+            fullData: data.list,
+            dailyData: dailyData,
+          },
+          () => console.log(this.state)
+        );
       });
   };
 
-  render() {
+  // map through an array of objects and create a card component for each obj
+  renderDailyCards = () => {
+    return this.state.dailyData.map((reading, index) => (
+      <DailyCard reading={reading} key={index} />
+    ));
+  };
 
+  render() {
     return (
       <div>
-        <h1>hello weather!</h1>
+        {this.renderDailyCards()}
       </div>
     );
   }
