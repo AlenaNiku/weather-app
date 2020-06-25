f86a72a7bbb67375fe35481615acd5c9e4524bea
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-24 20:49:27 -0400

    created a week container component

---
+++ src/WeekContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class WeekContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello weather!</h1>
+            </div>
+        )
+    }
+}
+
+export default WeekContainer
