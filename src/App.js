540f4f1800c2d498f0711c75ecd495c0375dbf17
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-24 21:06:07 -0400

    added an api key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import WeekContainer from './WeekContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <WeekContainer />
     </div>
   );
 }
--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 
 class WeekContainer extends Component {
     render() {
-        return (
-            <div>
-                <h1>hello weather!</h1>
-            </div>
-        )
-    }
+             
+    const apiKey = "bb92bc497a64f5f4cdcd15d73f3e55ee"
+
+    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=10022&units=imperial&APPID=${apiKey}`;
+    
+    return (
+      <div>
+        <h1>hello weather!</h1>
+      </div>
+    );
+  }
 }
 
 export default WeekContainer
+
+// this component will render 5 weather cards
