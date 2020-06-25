24dd32926abafa94a31fe446cb067f0ef74f5a98
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-24 22:18:45 -0400

    filtering api respose to be daily only by filtering for only readings at the same time of day

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 
 class WeekContainer extends Component {
 
+    state = {
+        fullData: [],
+        dailyData: []
+    }
+
   componentDidMount = () => {
 
     const apiKey = "bb92bc497a64f5f4cdcd15d73f3e55ee";
 
     const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=10022&units=imperial&APPID=${apiKey}`;
+
     fetch(weatherURL)
       .then((res) => res.json())
-      .then((data) => console.log(data.list));
+      .then(data => {
+          const dailyData = data.list.filter((reading) =>
+            reading.dt_txt.includes("18:00:00")
+          )
+          this.setState({
+              fullData: data.list,
+              dailyData: dailyData
+          }, () => console.log(this.state))
+      });
   };
 
   render() {
