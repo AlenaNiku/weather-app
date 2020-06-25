a2f044015561e35434bee8b5669d4a873f5bea2b
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-24 21:11:58 -0400

    fetching the weather data from open weather

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 class WeekContainer extends Component {
     render() {
              
-    const apiKey = "bb92bc497a64f5f4cdcd15d73f3e55ee"
+    const apiKey = "bb92bc497a64f5f4cdcd15d73f3e55ee";
 
-    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=10022&units=imperial&APPID=${apiKey}`;
+
+    this.componentDidMount = () => {
+        const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=10022&units=imperial&APPID=${apiKey}`;
+
+        fetch(weatherURL)
+            .then(resp => resp.json())
+            .then(data => console.log("Data list loaded:", data))
+
+    }
     
     return (
       <div>
